6a748c621b9feb7b3d90d7f8eb9b65a62b8147f9
devochka-valera <valeria.ryabenko@gmail.com>
2016-08-22 23:38:07 +0300

    extend styles for mobile version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,3 @@
 $(".skillbar").each(function(i, elem) {
     elem.childNodes[1].style.width = elem.childNodes[3].innerHTML;
 })
-
-
-
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: -34.397,
-            lng: 150.644
-        },
-        zoom: 8
-    });
-};
