21112bfbee9f93a3bd618e964203b25b03142e51
devochka-valera <valeria.ryabenko@gmail.com>
2016-08-24 00:17:09 +0300

    add navigation for mobile version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
-$(".skillbar").each(function(i, elem) {
+$('.skillbar').each(function(i, elem) {
     elem.childNodes[1].style.width = elem.childNodes[3].innerHTML;
-})
+});
+$(".nav__container").click(function() {
+    $(".nav__container").toggleClass("nav__container--open ");
+});
