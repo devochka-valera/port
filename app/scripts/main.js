83eaa2775e258e7c59370abd4e88ab3057ec61b8
devochka-valera <valeria.ryabenko@gmail.com>
2016-08-24 00:28:23 +0300

    refactor remove excessive nesting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
+// 
 $('.skillbar').each(function(i, elem) {
     elem.childNodes[1].style.width = elem.childNodes[3].innerHTML;
 });
-$(".nav__container").click(function() {
-    $(".nav__container").toggleClass("nav__container--open ");
+//  в мобильной версии по нажатию на header открываем выпадающее меню
+$(".nav__header").click(function() {
+    $(".nav__header").toggleClass("nav__header--open ");
 });
