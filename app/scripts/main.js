a9e0f7508bd2de8de783567366d4f1735263fc4a
devochka-valera <valeria.ryabenko@gmail.com>
2016-08-27 16:25:52 +0300

    change hero image, backgroung image, color palette

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,6 @@ $('.skillbar').each(function(i, elem) {
     elem.childNodes[1].style.width = elem.childNodes[3].innerHTML;
 });
 //  в мобильной версии по нажатию на header открываем выпадающее меню
-$(".nav__header").click(function() {
-    $(".nav__header").toggleClass("nav__header--open ");
+$('.nav__header').click(function() {
+    $('.nav__header').toggleClass('nav__header--open');
 });
