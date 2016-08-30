89d8c81aac6e763d2bb55530cf1299c9e49a6940
devochka-valera <valeria.ryabenko@gmail.com>
2016-08-30 23:58:37 +0300

    fixed incorrect jQery code about skillbar__percent

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,11 @@
-// 
+// ширина для skillbar относительно контента skillbar__percent
 $('.skillbar').each(function(i, elem) {
-    elem.childNodes[1].style.width = elem.childNodes[3].innerHTML;
+    $(elem).children(".skillbar__line").width($(elem).children(".skillbar__percent").html());
 });
+
+
+
+
 //  в мобильной версии по нажатию на header открываем выпадающее меню
 $('.nav__header').click(function() {
     $('.nav__header').toggleClass('nav__header--open');
