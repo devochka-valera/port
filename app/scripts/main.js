09c1e970cff4f7a6504cd01d82c9c264c2d098ce
devochka-valera <valeria.ryabenko@gmail.com>
2016-11-09 17:12:12 +0300

    add phone mask, add personal information, modal window & examples of works

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,47 +1,66 @@
-// ширина для skillbar относительно контента skillbar__percent
-$('.skillbar').each(function(i, elem) {
-    $(elem).children('.skillbar__line').width($(elem).children('.skillbar__percent').html());
-});
+$(function () {
+    var elementPosition = $('#skills').offset().top;
+    function renderSkills(){
+        var currentPosition = window.pageYOffset;
+        if (currentPosition > (elementPosition - $(window).height() * 0.75)
+             && currentPosition < (elementPosition + $('#skills').height())) {
+            $('.skillbar').each(function (i, elem) {
+                $(elem).children('.skillbar__line').width($(elem).children('.skillbar__percent').html());
+            });
+        }
+    }
 
+    $(window).on('scroll', renderSkills);
+    renderSkills();
 
+    //  в мобильной версии по нажатию на header открываем выпадающее меню
+    $('.nav__header').click(function () {
+        $('.nav__header').toggleClass('nav__header--open');
+    });
 
-
-//  в мобильной версии по нажатию на header открываем выпадающее меню
-$('.nav__header').click(function() {
-    $('.nav__header').toggleClass('nav__header--open');
-});
-
-// // галерея 
-$(function(img, e) {
-    // Cache variables
+    // // галерея 
     var $overlay = $('<div id="overlay"></div>'),
-        $image = $("<img>"),
-        $caption = $('<div class="work__description"></div>');
+        $image = $("<img id='popup_image'>"),
+        $caption = $('<div class="work__description"></div>'),
+        $container = $('<div class="modal__container"></div>');
 
-    // add image to overlay
-    $overlay.append($image);
-
+    // add container to overlay
+    $overlay.append($container);
+    $container.append($image);
     // add overlay
     $("body").append($overlay);
 
-    // Capture click event on a link to img
-    var imgLinks = $('#imageGallery a');
-
-    imgLinks.click(function(e) {
+    $('#imageGallery a').click(function (e) {
         e.preventDefault();
-        var imgLoc = $(this).attr('href');
-        $image.attr('src', imgLoc);
+        var imageLocation = $(this).attr('href');
+        $image.attr('src', imageLocation);
         //add caption to overlay
         $caption.insertAfter($image);
-        $image.addClass('borderedImg');
         $overlay.slideDown();
-
         var captionText = $(this).siblings('.work__description').html();
         $caption.html(captionText);
-
+        $image.on('load', function () {
+            $caption.width($image.width());
+        })
     });
 
-    $overlay.click(function() {
+    $overlay.click(function () {
         $overlay.slideUp();
     });
+
+    //Плавный скролл после нажатия на якорную ссылку
+    $("#menu").on("click", "li", function (event) {
+        event.preventDefault();
+        var id = $(this).children().attr('href');
+        var top = $(id).offset().top - 30;
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
+    });
+
+
+$(function(){
+$("#phone").mask("+7 (999) 999-9999");
+})
 });
